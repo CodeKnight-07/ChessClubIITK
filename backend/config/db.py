6ea460f1a6fcdd70ee46261ec0b344b2eb1b547b
Divyesh <6divyesh@gmail.com>
2026-06-24 17:32:39 +0530

    updated db.py

--- backend/config/db.py
+++ backend/config/db.py
@@ -4,7 +4,7 @@ import pymysql
 def get_db_connection():
     # Pure local development connection
     return pymysql.connect(
-        host=os.environ.get('DB_HOST'),
+        unix_socket=f"/cloudsql/{os.environ.get('INSTANCE_CONNECTION_NAME')}",
         user=os.environ.get('DB_USER'),
         password=os.environ.get('DB_PASSWORD'),
         db=os.environ.get('DB_NAME'),
